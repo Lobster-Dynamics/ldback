1a2eb322953ebceac3e535b639d44b8292aa02f2
Rodrigo Reyes <89705213+RodrigoReyes27@users.noreply.github.com>
2024-05-02 19:54:47 -0600

    Crear directorio

--- functions/app/directory/create_directory.py
+++ functions/app/directory/create_directory.py
@@ -8,16 +8,32 @@ from domain.directory import Directory
 from . import directory_blueprint
 
 
-@directory_blueprint.route('/get_directory/<id>', methods=['POST'])
-def create_directory_handle(id):
+@directory_blueprint.route('/create_directory', methods=['POST'])
+def create_directory_handle():
     token = request.token
 
+    data = request.get_json()
+    if not data:
+        return jsonify(msg="Directory id and name must be set"), 400
+    
+    name: str
+    directory_id: str
+    try:
+        name = data["name"]
+        directory_id = data["directory_id"]
+    except KeyError:
+        return jsonify(msg="Directory id and name must be set"), 400
+    
     # Reference directory repo
     repo = FirebaseDirectoryRepo()
 
+    # Create a new entry in the directory collection
     directory = Directory(
         id=uuid1(),
-        name="New Directory",
-        ownerId=token.user_id,
+        name=name,
+        ownerId=token["user_id"],
         containedItems=[]
     )
+    repo.add(directory)
+    
+    return jsonify(msg="Directory created successfully"), 200
--- functions/infrastructure/firebase/persistence/repos/directory_repo.py
+++ functions/infrastructure/firebase/persistence/repos/directory_repo.py
@@ -1,7 +1,6 @@
 from domain.directory import Directory
 from domain.directory.repo import IDirectoryRepo
 from firebase_admin import firestore
-from infrastructure.firebase.persistence.repos.document_repo import FirebaseDocumentRepo
 
 class FirebaseDirectoryRepo(IDirectoryRepo):
     def __init__(self):
@@ -9,7 +8,23 @@ class FirebaseDirectoryRepo(IDirectoryRepo):
         self.collection = self.db.collection('Directory')
         
     def add(self, item: Directory):
-        pass
+        directory = item.model_dump()
+        directory["id"] = str(directory["id"])
+
+        fields_to_exclude_as_collections = {
+            "containedItems",
+        }
+
+        main_directory_dict = {
+            key: value
+            for key, value in directory.items()
+            if key not in fields_to_exclude_as_collections
+        }
+
+        doc_ref = self.collection.document(directory["id"])
+
+        # Add the directory to the collection
+        doc_ref.set(main_directory_dict)
     
     def get(self, id: str):
         doc_ref = self.collection.document(id)
