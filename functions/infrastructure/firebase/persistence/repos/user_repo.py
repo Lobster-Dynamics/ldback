ae967d18f0c5375d7acda6c13c5b89b36971e908
Rodrigo Reyes <89705213+RodrigoReyes27@users.noreply.github.com>
2024-05-14 17:24:50 -0600

    Route to update user information

--- functions/infrastructure/firebase/persistence/repos/user_repo.py
+++ functions/infrastructure/firebase/persistence/repos/user_repo.py
@@ -19,4 +19,18 @@ class FirebaseUserRepo(IUserRepo):
         result = doc.to_dict()
         return User(**result)
     
-    def update(self, item: User): ...
+    def update(self, item: User):
+        doc_ref = self.collection.document(item.id)
+        doc = doc_ref.get()
+        
+        if not doc.exists:
+            raise ValueError("User not found")
+
+        try:
+            user = item.model_dump(by_alias=True)
+            updated_user= dict()
+            for key in user:
+                updated_user[key] = str(user[key])
+            doc_ref.update(updated_user)
+        except:
+            raise ValueError("Error updating user")
